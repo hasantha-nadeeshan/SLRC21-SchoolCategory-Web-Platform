c6007babe5114d8153facc26c682a25bae838f42
Ravindu Hiroshan <58454450+RavinduHiroshan@users.noreply.github.com>
2021-03-28 11:02:36 +0530

    functions update

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,7 @@ exports.submission = functions.storage.object().onFinalize(event => {
   const firstVal = 1;
   const time = event.timeCreated;
   const uid = event.metadata.uid;
-  const link = event.mediaLink;
+  const link = event.name;
   const task = event.metadata.task;
   const team = event.metadata.team;
   const db = admin.firestore();
@@ -49,19 +49,19 @@ exports.submission = functions.storage.object().onFinalize(event => {
   return getDoc;
 });
 
-exports.downloads = functions.https.onCall((data, contex) => {
-  const storage = admin.storage();
-  return new Promise((resolve, reject) => {
-   storage.bucket().file('Submission/Bymhw8vttmaidBhc7tIo5GP66Tc2/easy/clique wording-2.png');
-    httpsReference.getDownloadURL().then(doc => {
-    resolve(doc);
-    }).catch(reason => {
-      console.log('db.collection("colors").get gets err, reason: ' + reason);
-      reject(reason);
-  });
-  })
+// exports.downloads = functions.https.onCall((data, contex) => {
+//   const storage = admin.storage();
+//   return new Promise((resolve, reject) => {
+//    storage.bucket().file('Submission/Bymhw8vttmaidBhc7tIo5GP66Tc2/easy/clique wording-2.png');
+//     httpsReference.getDownloadURL().then(doc => {
+//     resolve(doc);
+//     }).catch(reason => {
+//       console.log('db.collection("colors").get gets err, reason: ' + reason);
+//       reject(reason);
+//   });
+//   })
     
-});
+// });
 
 exports.fireGetColors = functions.https.onCall((data, context) => {
   const task = data.taskNum
@@ -89,22 +89,18 @@ exports.fireGetColors = functions.https.onCall((data, context) => {
 
 
 
-exports.getUrl = functions.https.onCall(async (data, context) => {
-    const storage = new Storage();
-    // options of temporary access to file
-    const options = {
-        version: 'v2',
-        action: 'read',
-        expires: Date.now() + 1000 * 60 * 60, // expire date, one minute from now
-    };
-    // get a V2 signed URL for the file
-    const [url] = await storage
-        .bucket() // name of the storage bucket
-        .file('Submission/Bymhw8vttmaidBhc7tIo5GP66Tc2/easy/clique wording-2.png') // path of the file inside the bucket
-        .getSignedUrl(options); // returns a promise
-  return url;
-});
-
-
-
-
+// exports.getUrl = functions.https.onCall(async (data, context) => {
+//     const storage = new Storage();
+//     // options of temporary access to file
+//     const options = {
+//         version: 'v2',
+//         action: 'read',n
+//         expires: Date.now() + 1000 * 60 * 60, // expire date, one minute from now
+//     };
+//     // get a V2 signed URL for the file
+//     const [url] = await storage
+//         .bucket() // name of the storage bucket
+//         .file('Submission/Bymhw8vttmaidBhc7tIo5GP66Tc2/easy/clique wording-2.png') // path of the file inside the bucket
+//         .getSignedUrl(options); // returns a promise
+//   return url;
+// });
